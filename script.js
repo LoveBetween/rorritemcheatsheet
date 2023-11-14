16a634c651ac6e551c21bb90b2792102ad1eed1c
Christopher Jung <71386596+SeeJaeey@users.noreply.github.com>
2023-11-14 15:51:12 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-// script.js
+// style
 const itemName = document.getElementById('item-name');
 const description = document.getElementById('item-description');
 itemName.style.color = "white";
-itemName.style.textDecoration = "underline"
+itemName.style.textDecoration = "underline";
 description.style.color = "white";
 
+// dicts
+const itemDescriptionDict = {
+    "Arcane_Blades": "Increases movement speed by 30% (+30% per stack) after the Teleporter has been activated.",
+    "Backup_Magazine": "Add +1 (+1 per stack) charge of your Secondary skill.",
+    "Barbed_Wire": "Hurt 1 enemy within 1m (+0.2m per stack) for 50% (+10% per stack) damage every 0.5 seconds."
+}
+
+// helper functions
+function replaceAll(string, search, replace) {
+    return string.split(search).join(replace);
+}
+
+// actual functions
 function showDescription(filename) {
-    itemName.textContent = "Item Name";
-    description.textContent = "description blablabla";
+    itemName.textContent = replaceAll(filename, '_', ' ');
+    description.textContent = itemDescriptionDict.hasOwnProperty(filename) ? itemDescriptionDict[filename] : "Item Description missing :(";
 }
 
 function hideDescription(filename) {
