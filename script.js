aaf3a6b70e03e1d3b2e4a3fe87d4165fbb11b16b
Christopher Jung <71386596+SeeJaeey@users.noreply.github.com>
2023-11-14 01:45:22 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+const description = document.getElementById('item-description');
+
+function showDescription(itemName) {
+    description.textContent = "testasdasdasdasdsad";
+}
+
+function hideDescription(itemName) {
+    description.textContent = "";
+}
